7f6f637ed6a0ae7e8f7fe88d14e4fcb30a4ccf45
Daniel Cork <daniel@bysixteen.co.uk>
2025-07-25 13:15:40 +0100

    Hardcode APP_KEYS in server config to fix Railway deployment

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
+export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['5b9OfRZ5HRfZYgds4+YzZw==', 'w8qEOGSpYMT7+EtqLDC08Q==', '6vPgSTomMx8e7zHpvy0xuA==', 'KXVbyi8pZfQ6r+6+1E+uKA=='],
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
